746eae4b901f6b9f585d45b845502d55b62dc263
Khiet Le <khiet.le@ehochef.com>
2018-09-22 23:01:33 +0100

    add an IconButton to also handle submit

--- lib/main.dart
+++ lib/main.dart
@@ -36,11 +36,26 @@ class ChatScreenState extends State<ChatScreen> {
 
   Widget _buildTextComposer() {
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: TextField(
-        controller: _textController,
-        onSubmitted: _handleSubmitted,
-        decoration: InputDecoration.collapsed(hintText: "Send a message"),
+      margin: EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        children: <Widget>[
+          Flexible(
+            child: TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration: InputDecoration.collapsed(hintText: "Send a message"),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 4.0),
+            child: IconButton(
+              icon: Icon(Icons.send),
+              onPressed: () {
+                _handleSubmitted(_textController.text);
+              },
+            ),
+          )
+        ],
       ),
     );
   }
