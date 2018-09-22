24257ae8099e9a9b10863c80e27632276f046efc
Khiet Le <khiet.le@ehochef.com>
2018-09-22 22:25:03 +0100

    refactor code into widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(FriendlychatApp());
+}
+
+class FriendlychatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: "Friendlychat",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Friendlychat"),
-        ),
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Friendlychat"),
       ),
-    ),
-  );
+    );
+  }
 }
