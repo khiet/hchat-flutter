225f20339cb6cea49d1b97fbad7a148f476d96f2
Khiet Le <khiet.le@ehochef.com>
2018-09-24 09:14:35 +0100

    add a StatelessWidget to represent a chat message

--- lib/main.dart
+++ lib/main.dart
@@ -70,3 +70,36 @@ class ChatScreenState extends State<ChatScreen> {
     _textController.clear();
   }
 }
+
+const String _name = "Khiet Le";
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(child: Text(_name[0])),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(_name, style: Theme.of(context).textTheme.subhead),
+              Container(
+                margin: EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
