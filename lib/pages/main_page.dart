675cbfd404db801c3909fc45c4f06c762c1839c3
Khiet Le <khiet.le@ehochef.com>
2018-11-05 15:17:05 +0000

    add FCM and display a notification

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+
 import './home_page.dart';
 import './map_page.dart';
 import './history_page.dart';
@@ -17,6 +20,14 @@ class MainPageState extends State<MainPage> {
     HistoryPage(),
     MapPage(),
   ];
+  Widget _messageNotification = Container();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _setupFCM();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,9 +36,11 @@ class MainPageState extends State<MainPage> {
         title: Text("HChat"),
         elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
       ),
-      body: _pages[_tabIndex],
+      body: Stack(
+        children: <Widget>[_messageNotification, _pages[_tabIndex]],
+      ),
       bottomNavigationBar: BottomNavigationBar(
-        onTap: onTapHandler,
+        onTap: _onTapHandler,
         currentIndex: _tabIndex,
         items: [
           BottomNavigationBarItem(
@@ -47,9 +60,64 @@ class MainPageState extends State<MainPage> {
     );
   }
 
-  void onTapHandler(int tabIndex) {
+  Widget _buildMessageNotification(BuildContext context, String message) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      padding: EdgeInsets.all(10.0),
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: Theme.of(context).accentColor,
+        borderRadius: BorderRadius.all(
+          Radius.circular(
+            2.0,
+          ),
+        ),
+      ),
+      child: Row(
+        children: <Widget>[
+          Container(
+            child: Icon(
+              Icons.notifications_active,
+              color: Colors.white,
+            ),
+            margin: EdgeInsets.only(right: 5.0),
+          ),
+          Text(
+            message,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onTapHandler(int tabIndex) {
     setState(() {
       _tabIndex = tabIndex;
     });
+  }
+
+  void _setupFCM() async {
+    final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+    String token = await _firebaseMessaging.getToken();
+    print('[_setupFCM] $token');
+
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print('[_setupFCM] onMessage $message');
+        setState(() {
+          _messageNotification = _buildMessageNotification(
+              context, message['notification']['title']);
+        });
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print('[_setupFCM] onResume $message');
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print('[_setupFCM] onLaunch $message');
+      },
+    );
   }
 }
